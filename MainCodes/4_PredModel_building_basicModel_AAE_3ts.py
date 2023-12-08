a4a1e3e9c34f982e6125377c9ad495632748f355
Donghu Guo <donghu.guo21@imperial.ac.uk>
2023-12-08 16:23:56 +0000

    commit

--- MainCodes/4_PredModel_building_basicModel_AAE_3ts.py
+++ MainCodes/4_PredModel_building_basicModel_AAE_3ts.py
@@ -35,10 +35,9 @@ t_gaps_sampels = 5
 dt = 5
 ntimes = 3
 
-num_epochs = 1000 # 1000
+num_epochs = 6000 # 1000
 batch_size = 1  # Choose your desired batch size
-lr=0.005
-
+model_saved_path = root_path + 'Flow_Data/PredictionModel/autoencoder_Flow_PredictionLatent_{}epochs_AAEVen80Samples_3ts.pth'.format(num_epochs)
 
 samples_training = []
 samples_training_X = []
@@ -73,9 +72,6 @@ data = samples_training
 print(len(data))
 
 
-import torch
-from torch.utils.data import Dataset, DataLoader
-
 class MyDataset(Dataset):
     def __init__(self, data_x, data_y):
         self.data_x = data_x
@@ -96,40 +92,103 @@ shuffle = False  # Set to True if you want to shuffle the data during training
 
 data_loader = DataLoader(my_dataset, batch_size=batch_size, shuffle=shuffle)
 
+# ## ############################## ## #######################
+# ## #######################Old Model
+# # Encoder and Decoder (same as before)
+# class Encoder(nn.Module):
+#     def __init__(self):
+#         super(Encoder, self).__init__()
+#         self.encoder = nn.Sequential(
+#             nn.Conv2d(ss_0.shape[1], 64, kernel_size=5, stride=1, padding=2),
+#             nn.ReLU(),
+#             nn.BatchNorm2d(64),
+#             nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2),
+#             nn.ReLU(),
+#             nn.BatchNorm2d(128),
+#             # Add more convolutional layers as needed
+#         )
 
-# Encoder and Decoder (same as before)
+#     def forward(self, x):
+#         return self.encoder(x)
+
+# class Decoder(nn.Module):
+#     def __init__(self):
+#         super(Decoder, self).__init__()
+#         self.decoder = nn.Sequential(
+#             nn.Conv2d(128, 64, kernel_size=5, stride=1, padding=2),
+#             nn.ReLU(),
+#             nn.BatchNorm2d(64),
+#             nn.Conv2d(64, ss_1.shape[1], kernel_size=5, stride=1, padding=2),
+#             nn.Identity(),
+#             # Add more convolutional layers as needed
+#         )
+
+#     def forward(self, x):
+#         return self.decoder(x)
+
+# # Discriminator
+# class Discriminator(nn.Module):
+#     def __init__(self):
+#         super(Discriminator, self).__init__()
+#         self.discriminator = nn.Sequential(
+#             nn.Conv2d(128, 64, kernel_size=5, stride=1, padding=2),
+#             nn.ReLU(),
+#             nn.BatchNorm2d(64),
+#             nn.Conv2d(64, 1, kernel_size=5, stride=1, padding=2),
+#             nn.Sigmoid()
+#         )
+
+#     def forward(self, x):
+#         return self.discriminator(x)
+
+# # Adversarial Autoencoder (AAE)
+# class AdversarialAutoencoder(nn.Module):
+#     def __init__(self, encoder, decoder, discriminator):
+#         super(AdversarialAutoencoder, self).__init__()
+#         self.encoder = encoder
+#         self.decoder = decoder
+#         self.discriminator = discriminator
+
+#     def forward(self, x):
+#         encoded = self.encoder(x)
+#         decoded = self.decoder(encoded)
+#         discriminated = self.discriminator(encoded)
+#         return decoded, discriminated
+
+
+# ## ############################## ## #######################
+# ## #######################AAE Model from Ventilation
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
-        self.encoder = nn.Sequential(
-            nn.Conv2d(ss_0.shape[1], 64, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            # Add more convolutional layers as needed
+        self.model = nn.Sequential(
+            nn.Conv2d(ss_0.shape[1], 256, kernel_size=(3, 3), stride=1, padding=1),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(256, 128, kernel_size=(3, 3), stride=1, padding=1),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(128, 128, kernel_size=(3, 3), stride=1, padding=1),
+            nn.LeakyReLU()
         )
 
     def forward(self, x):
-        return self.encoder(x)
+        return self.model(x)
 
 class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
-        self.decoder = nn.Sequential(
-            nn.Conv2d(128, 64, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, ss_1.shape[1], kernel_size=5, stride=1, padding=2),
-            nn.Identity(),
-            # Add more convolutional layers as needed
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=(3, 3), stride=1, padding=1, output_padding=0, bias=False),
+            nn.LeakyReLU(),
+
+            nn.ConvTranspose2d(64, ss_1.shape[1], kernel_size=(3, 3), stride=1, padding=1, output_padding=0, bias=False),
+            nn.Identity()
         )
 
     def forward(self, x):
-        return self.decoder(x)
+        return self.model(x)
 
-# Discriminator
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
@@ -143,7 +202,7 @@ class Discriminator(nn.Module):
 
     def forward(self, x):
         return self.discriminator(x)
-
+    
 # Adversarial Autoencoder (AAE)
 class AdversarialAutoencoder(nn.Module):
     def __init__(self, encoder, decoder, discriminator):
@@ -157,89 +216,102 @@ class AdversarialAutoencoder(nn.Module):
         decoded = self.decoder(encoded)
         discriminated = self.discriminator(encoded)
         return decoded, discriminated
+    
 
-# Instantiate components
-encoder = Encoder()
-decoder = Decoder()
-discriminator = Discriminator()
-aae_model = AdversarialAutoencoder(encoder, decoder, discriminator)
+
+
+
+# Instantiate components and move to GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+encoder = Encoder().to(device)
+decoder = Decoder().to(device)
+discriminator = Discriminator().to(device)
+aae_model = AdversarialAutoencoder(encoder, decoder, discriminator).to(device)
 
 # Loss functions
-criterion_autoencoder = nn.MSELoss()
-criterion_adversarial = nn.BCELoss()
+reconstruction_loss_fn = nn.MSELoss()
+adversarial_loss_fn = nn.BCELoss()
 
 # Optimizers
-optimizer_aae = optim.Adam(aae_model.parameters(), lr=lr, betas=(0.5, 0.999))
+lr_ae = 0.001
+lr_d = 0.001
 
-# Initialize lists to store losses and epochs for plotting
-all_losses = []
-epochs = []
+optimizer_ae = optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=lr_ae)
+optimizer_d = optim.Adam(discriminator.parameters(), lr=lr_d)
 
+# Lists to store losses for plotting
+all_losses_ae = []
+all_losses_d = []
 
-# Initialize lists to store losses and epochs for plotting
-all_losses_autoencoder = []
-all_losses_discriminator = []
-epochs = []
-
-
+# Print the GPU device
+print(f"Running on GPU: {torch.cuda.get_device_name(torch.cuda.current_device())}")
 # Training loop
 for epoch in range(num_epochs):
+    epoch_losses_ae = []
+    epoch_losses_d = []
+
     for batch in data_loader:
-        inputs, targets = batch['input'], batch['target']
+        inputs, targets = batch['input'].to(device), batch['target'].to(device)
 
         # Train autoencoder
-        optimizer_aae.zero_grad()
+        optimizer_ae.zero_grad()
         decoded, discriminated = aae_model(inputs)
-        loss_autoencoder = criterion_autoencoder(decoded, targets)
-        loss_adversarial = criterion_adversarial(discriminated, torch.ones_like(discriminated))
-        total_loss = loss_autoencoder + loss_adversarial
-        total_loss.backward()
-        optimizer_aae.step()
-    
-    # Append the losses to the lists for plotting
-    all_losses_autoencoder.append(loss_autoencoder.item())
-    all_losses_discriminator.append(loss_adversarial.item())
-    epochs.append(epoch + 1)
+        loss_ae = reconstruction_loss_fn(decoded, targets)
+        loss_ae.backward()
+        optimizer_ae.step()
+        epoch_losses_ae.append(loss_ae.item())
 
-    # Print the losses at the end of each epoch
-    print(f'Epoch [{epoch+1}/{num_epochs}], Autoencoder Loss: {loss_autoencoder.item():.4f}, Discriminator Loss: {loss_adversarial.item():.4f}')
+        # Train discriminator
+        optimizer_d.zero_grad()
+        with torch.no_grad():
+            encoded_fake = encoder(inputs).detach()
+            encoded_true = torch.randn_like(encoded_fake).to(device)   # the true sample
+        fake_output = discriminator(encoded_fake)
+        true_output = discriminator(encoded_true)
+        loss_d = adversarial_loss_fn(fake_output, torch.zeros_like(fake_output).to(device)) + \
+                 adversarial_loss_fn(true_output, torch.ones_like(true_output).to(device))
+        loss_d.backward()
+        optimizer_d.step()
+        epoch_losses_d.append(loss_d.item())
+
+    # Average losses over the epoch
+    avg_loss_ae = sum(epoch_losses_ae) / len(epoch_losses_ae)
+    avg_loss_d = sum(epoch_losses_d) / len(epoch_losses_d)
+
+    # Print the average loss at the end of each epoch
+    print(f'Epoch [{epoch+1}/{num_epochs}], AE Loss: {avg_loss_ae:.4f}, D Loss: {avg_loss_d:.4f}')
+
+    # Record the losses for plotting
+    all_losses_ae.append(avg_loss_ae)
+    all_losses_d.append(avg_loss_d)
+
+# Plot the losses separately
+fig = plt.figure(figsize=(10, 5))
+
+# Autoencoder Loss Plot
+plt.subplot(1, 2, 1)
+plt.plot(all_losses_ae, label='Autoencoder Loss')
+plt.title('Autoencoder Loss Over Epochs')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+# Discriminator Loss Plot
+plt.subplot(1, 2, 2)
+plt.plot(all_losses_d, label='Discriminator Loss', color='orange')
+plt.title('Discriminator Loss Over Epochs')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.tight_layout()
+fig.savefig(model_saved_path[:-5]+'.png')
+plt.close(fig)
 
 print("Training finished.")
+aae_model = aae_model.cpu()
+print("Moved model back to cpu.")
 
-model_saved_path = root_path + 'Flow_Data/PredictionModel/autoencoder_Flow_PredictionLatent_{}epochs_AAE80Samples_3ts.pth'.format(num_epochs)
 torch.save(aae_model.state_dict(), model_saved_path)  ### PredictionMulti
 print("Finished model: ", model_saved_path)
-
-# Plot the losses
-fig = plt.figure(figsize=(10, 5))
-
-plt.subplot(1, 2, 1)
-plt.plot(epochs, all_losses_autoencoder, label='Autoencoder Loss')
-plt.title('Autoencoder Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs, all_losses_discriminator, label='Discriminator Loss')
-plt.title('Discriminator Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.tight_layout()
-fig.savefig(model_saved_path[:-5]+'1.png')
-plt.close(fig)
-
-
-fig = plt.figure(figsize=(10, 5))
-plt.plot(epochs, all_losses_autoencoder, label='Autoencoder Loss')
-plt.plot(epochs, all_losses_discriminator, label='Discriminator Loss')
-plt.title('Autoencoder and Discriminator Losses')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.tight_layout()
-fig.savefig(model_saved_path[:-5]+'2.png')
-plt.close(fig)
