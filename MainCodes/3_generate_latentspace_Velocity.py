098ca40499014d1ed2036d18b820eee95a46a776
Donghu Guo <donghu.guo21@imperial.ac.uk>
2024-03-18 21:48:56 +0000

    0318 renames and add convlstm

--- MainCodes/3_generate_latentspace_Velocity.py
+++ MainCodes/3_generate_latentspace_Velocity.py
@@ -11,58 +11,76 @@ os.environ["CUDA_VISIBLE_DEVICES"] = f"{4}"
 
 
 ### 256x256
-# datasetFolder = 'Flow_Data'
-# xysize = 256
-# ntimesteps = 501
-# samples = []
-# for i in range(ntimesteps):
-#     s = np.load('/home/dg321/gitTest/PRI/irp/FlowPassBuilding/' + datasetFolder + '/InterpolatedResult256/InterpolatedResult256Raw/FpB_Interpolated_t{}_Velocity_{}_{}.npy'.format(
-#         i, xysize, xysize))
-#     samples.append(s)
-#     print(s.shape)
-
-## 384x384
-datasetFolder = 'Flow_Data_9_9'
-xysize = 384
-ntimesteps = 399
+datasetFolder = 'Flow_Data'
+xysize = 256
+ntimesteps = 501
 samples = []
 for i in range(ntimesteps):
-    s = np.load('/home/dg321/gitTest/PRI/irp/FlowPassBuilding/' + datasetFolder + '/FpB_Interpolated_Velocity_384_384/FpB_Interpolated_t{}_Velocity_{}_{}.npy'.format(i, xysize, xysize))
+    s = np.load('/home/dg321/gitTest/PRI/irp/FlowPassBuilding/' + datasetFolder + '/InterpolatedResult256/InterpolatedResult256Raw/FpB_Interpolated_t{}_Velocity_{}_{}.npy'.format(
+        i, xysize, xysize))
     samples.append(s)
+    print(s.shape)
 
-fist_hidden_layer = 16
-second_hidden_layer = 32
-latent_channel_number = 4
+## 384x384
+# datasetFolder = 'Flow_Data_9_9'
+# xysize = 384
+# ntimesteps = 399
+# samples = []
+# for i in range(ntimesteps):
+#     s = np.load('/home/dg321/gitTest/PRI/irp/FlowPassBuilding/' + datasetFolder + '/FpB_Interpolated_Velocity_384_384/FpB_Interpolated_t{}_Velocity_{}_{}.npy'.format(i, xysize, xysize))
+#     samples.append(s)
 
+# Define the autoencoder model
+hid1 = 20*2
+hid2= 40*2
+hid3 = 80*2
+hid4 = 80*2
+hid5 = 40*2
+hid6 = 20*2
+            
 class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
-
-        # Encoder layers
+ 
+        # Encoder
         self.encoder = nn.Sequential(
-            nn.Conv2d(2, fist_hidden_layer, kernel_size=2, stride=2, padding=0),  
+            nn.Conv2d(2, hid1, kernel_size=3, stride=1, padding=1),#1 - Smoothing the change in number of channels; no reduction in output size
             nn.LeakyReLU(negative_slope=0.2),
-            nn.Conv2d(fist_hidden_layer, second_hidden_layer, kernel_size=2, stride=2, padding=0),  
+            nn.Conv2d(hid1, hid2, kernel_size=2, stride=2),#2 - reduction by 2
             nn.LeakyReLU(negative_slope=0.2),
-            nn.Conv2d(second_hidden_layer, fist_hidden_layer, kernel_size=2, stride=1, padding=1),  
+            nn.Conv2d(hid2, hid3, kernel_size=3, stride=1, padding=1),#3 - Smoothing the change in number of channels; no reduction in output size
             nn.LeakyReLU(negative_slope=0.2),
-            nn.Conv2d(fist_hidden_layer, latent_channel_number, kernel_size=2, stride=1, padding=0),  
+            nn.Conv2d(hid3, hid4, kernel_size=2, stride=2),#4 - reduction by 2
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(hid4, hid5, kernel_size=3, stride=1, padding=1),#5 - Smoothing the change in number of channels; no reduction in output size
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(hid5, hid6, kernel_size=2, stride=2),#6 - reduction by 2
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.Conv2d(hid6, 2, kernel_size=3, stride=1, padding=1),#7 - Smoothing the change in number of channels; no reduction in output size
+            #We should finish with the same number of channels of the input (4)
             # nn.LeakyReLU(negative_slope=0.2)
-            nn.Tanh()  # constrain the latent space from range -1 to 1, Change this to nn.Sigmoid() for range 0 to 1
+            nn.Tanh()
         )
-
-        # Decoder layers
+ 
+        # Decoder
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(latent_channel_number, fist_hidden_layer, kernel_size=2, stride=1, padding=0),  
+            nn.ConvTranspose2d(2, hid6, kernel_size=3, stride=1, padding=1),# Smoothing the change in number of channels; no increase in output size
             nn.LeakyReLU(negative_slope=0.2),
-            nn.ConvTranspose2d(fist_hidden_layer, second_hidden_layer, kernel_size=2, stride=1, padding=1),  
+            nn.ConvTranspose2d(hid6, hid5, kernel_size=2, stride=2),
             nn.LeakyReLU(negative_slope=0.2),
-            nn.ConvTranspose2d(second_hidden_layer, fist_hidden_layer, kernel_size=2, stride=2, padding=0),  
+            nn.ConvTranspose2d(hid5, hid4, kernel_size=3, stride=1, padding=1),# Smoothing the change in number of channels; no increase in output size
             nn.LeakyReLU(negative_slope=0.2),
-            nn.ConvTranspose2d(fist_hidden_layer, 2, kernel_size=2, stride=2, padding=0),   
+            nn.ConvTranspose2d(hid4, hid3, kernel_size=2, stride=2),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.ConvTranspose2d(hid3, hid2, kernel_size=3, stride=1, padding=1),# Smoothing the change in number of channels; no increase in output size
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.ConvTranspose2d(hid2, hid1, kernel_size=2, stride=2),
+            nn.LeakyReLU(negative_slope=0.2),
+            nn.ConvTranspose2d(hid1, 2, kernel_size=3, stride=1, padding=1),# Smoothing the change in number of channels; no increase in output size
             nn.Identity()
+            #nn.Sigmoid()
         )
-
+ 
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
@@ -79,7 +97,7 @@ print(autoencoder)
 
 # Load the saved autoencoder
 run = wandb.init()
-artifact = run.use_artifact('guodh/compression - 2 latent channels/model:v3', type='model')
+artifact = run.use_artifact('guodh/compression - Containing kernel size 3/model:v8', type='model')
 artifact_dir = artifact.download()
 
 # Load the model
@@ -123,7 +141,7 @@ latent_samples_stacked = np.stack(latent_samples)
 
 print(latent_samples_stacked.shape)
 
-savepath = '/home/dg321/gitTest/PRI/irp/FlowPassBuilding/' + datasetFolder + '/Latent_data_Velocity_{}_{}_49696.npy'.format(xysize, xysize)
+savepath = '/home/dg321/gitTest/PRI/irp/FlowPassBuilding/' + datasetFolder + '/Latent_data_Velocity_{}_{}_23232.npy'.format(xysize, xysize)
 
 np.save(savepath, latent_samples_stacked)
 print('Finished: ' + savepath)
